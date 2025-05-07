7733e8b438d86003377e939b24764841656984b3
0xDLucky <128000262+0xDlucky@users.noreply.github.com>
2025-05-06 23:29:37 -0300

    Delete lib directory

--- lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constants {
-  static const Color primaryColor = Colors.blue;
-  static const Color accentColor = Colors.blueAccent;
-  static const double defaultPadding = 16.0;
-  static const double defaultBorderRadius = 10.0;
-  static const String genericErrorMessage = 'Ocorreu um erro. Tente novamente.';
-}
